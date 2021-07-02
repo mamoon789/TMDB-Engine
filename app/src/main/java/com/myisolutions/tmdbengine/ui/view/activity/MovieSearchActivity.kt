83af075d4cafb7240773011b1079c79bd9e83130
mamoonasad <74287953+mamoonasad@users.noreply.github.com>
2021-07-02 17:30:12 +0500

    Project Setup

---
+++ app/src/main/java/com/myisolutions/tmdbengine/ui/view/activity/MovieSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.myisolutions.tmdbengine.ui.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myisolutions.tmdbengine.R
+
+class MovieSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/myisolutions/tmdbengine/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.myisolutions.tmdbengine.util
+
+object Constants {
+    const val API_KEY = "e9fc384a407364993d8c7c0c128d3e40"
+}
